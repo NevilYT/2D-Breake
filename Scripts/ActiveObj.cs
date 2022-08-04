2f0298b8af1195f3e45ce6f22be02dfa90f18b98
Emmonn <57413233+NevilYT@users.noreply.github.com>
2022-08-04 15:08:48 +0100

    Add files via upload

---
+++ Scripts/ActiveObj.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObj : MonoBehaviour
+{
+    public GameObject hideobj;
+    private bool ison = true;
+    public GameObject onmenu;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ClickButt(){
+        ison = !ison;
+        hideobj.SetActive(ison);
+        onmenu.SetActive(!ison);
+        ison = !ison;
+    }
+}
